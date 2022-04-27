e605b713c3fc720c66ff1c62b9941074d9ac66a8
Anuraag Mazumdar <anuraagm97@gmail.com>
2022-04-27 13:15:46 +0530

    first-commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Shopping",
+        day: "28.11.2021",
+        reminder: true
+    },
+    {
+        id: 1,
+        text: "Meeting",
+        day: "29.11.2021",
+        reminder: true
+    },
+    {
+        id: 1,
+        text: "College",
+        day: "30.11.2021",
+        reminder: false
+    }
+];
